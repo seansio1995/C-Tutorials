f22388c2050348f713040cc62bc403323f432900
seansio1995 <seansio@163.com>
2016-04-26 14:43:23 +0800

    sqrt func

---
+++ functions/sqrt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double num;
+	cout<<"Enter a number:"<<endl;
+	cin>>num;
+	cout<<"The square root of "<<num<<" is "<<sqrt(num)<<endl;
+	return 0;
+}

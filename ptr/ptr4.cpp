6d9c79c0fdbb3ea88cd6cfb4777883b5588121e0
seansio1995 <seansio@163.com>
2016-04-27 14:30:51 +0800

    failed test of addrs

---
+++ ptr/ptr4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	a=10;
+	b=20;
+
+	int tmp; //tmp container
+	cout<<"Addrs now:"<<endl;
+	cout<<"a addr: "<<&a<<endl;
+	cout<<"b addr: "<<&b<<endl;
+
+	tmp=a;
+	a=b;
+	b=tmp;
+
+	cout<<endl<<"Addrs after:"<<endl;
+	cout<<"a addr: "<<&a<<endl;
+	cout<<"b addr: "<<&b<<endl;
+	return 0;
+}

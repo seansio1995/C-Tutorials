a60b71e19c25378e059cbaf4ed7ce7dd4ef998f6
seansio1995 <seansio@163.com>
2016-04-27 11:23:34 +0800

    second commit

--- functions/displayArray.cpp
+++ functions/displayArray.cpp
@@ -6,12 +6,12 @@ void display(int[],int);
 int main()
 {
 	int array[5]={1,2,3,4,5};
-	display(array,5);
+	display(array,5 );
 	return 0;
 }
 
 
-void display(int arr[],int size)
+void display(int arr[], int size)
 {
 	for(int i=0;i<size;i++)
 	{
